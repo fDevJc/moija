27b9177815634182be6ec8fd03205e47a1e14d4f
fDevJc <fDevJc@gmail.com>
2021-03-28 20:45:34 +0900

    modified source

--- client/src/routes/GameDetail.js
+++ client/src/routes/GameDetail.js
@@ -4,6 +4,13 @@ const GameDetail = () => {
   return (
     <>
       <div>GameDetail</div>
+      <div>장소</div>
+      <div>날짜</div>
+      <div>시간</div>
+      <div>
+        <button>참가하기</button>
+        <button>뒤로가기</button>
+      </div>
     </>
   );
 };
